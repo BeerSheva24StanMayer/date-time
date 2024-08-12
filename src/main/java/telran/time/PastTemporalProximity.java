b33f8f88261e47b4c5e3aff128231a349004ddfa
Stan Mayer <sthumor@gmail.com>
2024-08-12 17:03:32 +0300

    hw_date_time_1st

---
+++ src/main/java/telran/time/PastTemporalProximity.java
@@ -0,0 +1,74 @@
+package telran.time;
+
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.util.Arrays;
+
+public class PastTemporalProximity implements TemporalAdjuster {
+    // TODO some encapsulation
+    // array of temporals supporting Day, Month, Year (Dates)
+    Temporal[] dates;
+
+    public PastTemporalProximity(Temporal[] dates) {
+        this.dates = dates;
+    }
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+        // TODO Auto-generated method stub
+        // return the temporal for the encapsulated array
+        // that is a nearest in past
+        Temporal finalDate = null;
+        boolean temporalDateOk = isOkWithDateTem(temporal);
+        boolean temporalArrDateOk = isOkWithDateAr(dates);
+        if (temporalArrDateOk && temporalDateOk) {
+            convert(dates, temporal);
+            Arrays.sort(dates);
+            finalDate = nearestNegative(dates, temporal);
+        }
+        return finalDate;
+    }
+
+    private void convert(Temporal[] ar, Temporal temp) {
+        for (int i = 0; i < ar.length; i++) {
+            long between = temp.until(dates[i], ChronoUnit.DAYS);
+            ar[i] = temp.plus(between, ChronoUnit.DAYS);
+        }
+    }
+
+    private Temporal nearestNegative(Temporal[] ar, Temporal temp) {
+        int start = 0;
+        int end = ar.length - 1;
+        int middle = (start + end) / 2;
+        while (start <= end) {
+            long diff = temp.until(dates[middle], ChronoUnit.DAYS);
+            if (diff >= 0) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+            middle = (start + end) / 2;
+        }
+        return end == -1 ? null : ar[end];
+    }
+
+    private boolean isOkWithDateAr(Temporal[] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            boolean year = ar[i].isSupported(ChronoUnit.YEARS);
+            boolean month = ar[i].isSupported(ChronoUnit.MONTHS);
+            boolean day = ar[i].isSupported(ChronoUnit.DAYS);
+            if (!year && !month && !day) {
+                ar[i] = null;
+            }
+        }
+        return ar.length > 0;
+    }
+
+    private boolean isOkWithDateTem(Temporal temporal) {
+        boolean year = temporal.isSupported(ChronoUnit.YEARS);
+        boolean month = temporal.isSupported(ChronoUnit.MONTHS);
+        boolean day = temporal.isSupported(ChronoUnit.DAYS);
+        return year & month & day;
+    }
+}
---
+++ src/test/java/telran/time/DateTimeTest.java
@@ -0,0 +1,72 @@
+package telran.time;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.*;
+import java.time.chrono.MinguoDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
+import java.util.Locale;
+
+import org.junit.jupiter.api.Test;
+
+public class DateTimeTest {
+    @Test
+    void localDateTest() {
+        LocalDate current = LocalDate.now();
+        LocalDateTime currentTime = LocalDateTime.now();
+        ZonedDateTime currentZonedTime = ZonedDateTime.now();
+        Instant currentInstant = Instant.now();
+        LocalTime currentLtime = LocalTime.now();
+        System.out.printf("Current date %s in ISO format \n", current);
+        System.out.printf("Current date & time %s in ISO format \n", currentTime);
+        System.out.printf("Current zoned date and time %s in ISO format \n", currentZonedTime);
+        System.out.printf("Current instant of date & time %s in ISO format \n", currentInstant);
+        System.out.printf("Local current of date & time %s in ISO format \n", currentLtime);
+        System.out.printf("Current date is %s in dd/mm/yyyy \n",
+                current.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+    }
+
+    @Test
+    void nextFridayTest() {
+        LocalDate current = LocalDate.of(2024, 8, 11);
+        LocalDate expected = LocalDate.of(2024, 9, 13);
+        NextFriday13 adjuster = new NextFriday13();
+        assertEquals(expected, current.with(adjuster));
+        assertThrows(RuntimeException.class, () -> LocalTime.now().with(adjuster));
+    }
+
+    @Test
+    void pastTempProxTest() {
+
+        Temporal[] datesWithPast = { LocalDateTime.of(2024, 7, 8, 12, 10, 10),
+                ZonedDateTime.of(2024, 7, 9, 10, 4, 5, 5, ZoneId.of("Europe/Berlin")),
+                LocalDate.of(2023, 9, 13), MinguoDate.of(2024, 8, 8), LocalDate.of(1915, 9, 10) };
+
+        Temporal[] datesWithoutPast = { LocalDateTime.of(2025, 7, 8, 12, 10, 10),
+                ZonedDateTime.of(2024, 9, 9, 10, 4, 5, 5, ZoneId.of("Europe/Berlin")),
+                LocalDate.of(2024, 12, 13), MinguoDate.of(2024, 10, 8),
+                LocalDate.of(2027, 9, 10) };
+
+        PastTemporalProximity adjasterWithNegative = new PastTemporalProximity(datesWithPast);
+        PastTemporalProximity adjusterWithoutNegative = new PastTemporalProximity(datesWithoutPast);
+
+        //Test for localDate as comparable
+        LocalDate localDate = LocalDate.of(2024, 8, 12);
+        assertEquals(LocalDate.of(2024, 7, 9), localDate.with(adjasterWithNegative));
+        assertEquals(null, localDate.with(adjusterWithoutNegative));
+
+        //Test for MinguoDate as comparable
+        MinguoDate minguoDate = MinguoDate.of(113, 8, 12);
+        assertEquals(MinguoDate.of(113, 7, 9), minguoDate.with(adjasterWithNegative));
+        assertThrows(RuntimeException.class, () -> minguoDate.with(adjusterWithoutNegative)); // I MinguoDate can't be Null
+
+        //Test for MinguoDate as comparable
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 8, 12, 10, 00, 12);
+        assertThrows(RuntimeException.class, () -> localDateTime.with(adjasterWithNegative));
+        assertThrows(RuntimeException.class, () -> localDateTime.with(adjasterWithNegative));
+    }
+
+}
